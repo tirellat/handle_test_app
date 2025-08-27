a41996f7755a56c70128b182c9502c7e2f69d283
Terence Tirella <terence.tirella@shopify.com>
2025-08-27 16:15:28 -0400

    Initial commit

---
+++ extensions/ext-pos-ui-draft-order-details/src/Action.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {
+  Text,
+  Screen,
+  ScrollView,
+  Navigator,
+  reactExtension,
+  useApi,
+} from '@shopify/ui-extensions-react/point-of-sale';
+
+const Modal = () => {
+  const api = useApi<"pos.draft-order-details.action.render">();
+  
+  return (
+    <Navigator>
+      <Screen name="DraftOrderDetailsAction" title="Draft Order Details Action">
+        <ScrollView>
+          <Text>{`Draft Order ID: ${api.draftOrder.id}`}</Text>
+        </ScrollView>
+      </Screen>
+    </Navigator>
+  );
+};
+
+export default reactExtension('pos.draft-order-details.action.render', () => (
+  <Modal />
+));
---
+++ extensions/ext-pos-ui-draft-order-details/src/Block.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  Text,
+  useApi,
+  reactExtension,
+  POSBlock,
+  POSBlockRow,
+} from '@shopify/ui-extensions-react/point-of-sale';
+
+const Block = () => {
+  const api = useApi<"pos.draft-order-details.block.render">();
+  
+  return (
+    <POSBlock action={{title: 'Open action', onPress: api.action.presentModal}}>
+      <POSBlockRow>
+        <Text>{'This is a block extension'}</Text>
+        <Text>{`Draft Order ID for this draft order: ${api.draftOrder.id}`}</Text>
+      </POSBlockRow>
+    </POSBlock>
+  );
+};
+
+export default reactExtension('pos.draft-order-details.block.render', () => (
+  <Block />
+));
