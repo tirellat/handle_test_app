4442fc896590b3e7a5340d9a59e10e83ff5188bc
Terence Tirella <terence.tirella@shopify.com>
2025-08-27 16:43:18 -0400

    delete extensions that didn't build, backup env file

--- extensions/ext-admin-purchase-option/src/ProductExtension.jsx
+++
@@ -1,6 +0,0 @@
-import {reactExtension} from '@shopify/ui-extensions-react/admin';
-import PurchaseOptionsActionExtension from './PurchaseOptionsActionExtension';
-
-export default reactExtension('admin.product-purchase-option.action.render', () => (
-  <PurchaseOptionsActionExtension extension="admin.product-purchase-option.action.render" />
-));
